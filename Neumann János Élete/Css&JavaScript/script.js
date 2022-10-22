d0385ada195f75620c75d866b7aa7785d506c5e0
SNRobi <97282809+SNRobi@users.noreply.github.com>
2022-10-22 20:27:18 +0200

    Fáljok átrendezése

---
+++ Neumann János Élete/Css&JavaScript/script.js
@@ -0,0 +1,7 @@
+document.getElementById("kep").onmousemove = function() {
+    document.getElementById("kep").classList.add("img-thumbnail", "boxshadow");
+}
+document.getElementById("kep").onmouseleave = function() {
+    document.getElementById("kep").classList.remove("img-thumbnail", "boxshadow")
+}
+
