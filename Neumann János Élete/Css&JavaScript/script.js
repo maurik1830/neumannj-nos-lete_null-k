fdb4af90340e9eddc4a9e55e1dc3b86d008665c0
maurik1830 <nagyemoric@gmail.com>
2022-10-26 16:45:22 +0200

    Neumann János élete (rendezett)

--- Neumann János Élete/Css&JavaScript/script.js
+++ Neumann János Élete/Css&JavaScript/script.js
@@ -5,3 +5,25 @@ document.getElementById("kep").onmouseleave = function() {
     document.getElementById("kep").classList.remove("img-thumbnail", "boxshadow")
 }
 
+
+
+
+
+document.getElementById("kep1").onmousemove = function() {
+    document.getElementById("kep1").classList.add("img-thumbnail", "boxshadow");
+}
+document.getElementById("kep1").onmouseleave = function() {
+    document.getElementById("kep1").classList.remove("img-thumbnail", "boxshadow")
+}
+
+
+
+
+
+document.getElementById("kep2").onmousemove = function() {
+    document.getElementById("kep2").classList.add("img-thumbnail", "boxshadow");
+}
+document.getElementById("kep2").onmouseleave = function() {
+    document.getElementById("kep2").classList.remove("img-thumbnail", "boxshadow")
+}
+
